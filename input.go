052d98dd8ce877eb6ec53d8e44f9db0090f32586
Dan LaManna <dan.lamanna@kitware.com>
2017-07-26 19:08:42 -0400

    wip

--- input.go
+++ input.go
@@ -1,29 +1,40 @@
 package main
 
 import (
-	"github.com/adammck/venv"
-	"github.com/blang/vfs"
+        "fmt"
+        "github.com/adammck/venv"
+        "github.com/blang/vfs"
+        "io/ioutil"
 )
 
 func GetInputPath(fs vfs.Filesystem, env venv.Env) string {
 
-	var fn string
+        var fn string
 
-	fn = env.Getenv("TF_STATE")
-	if fn != "" {
-		return fn
-	}
+        fn = env.Getenv("TF_STATE")
+        if fn != "" {
+                return fn
+        }
 
-	fn = env.Getenv("TI_TFSTATE")
-	if fn != "" {
-		return fn
-	}
+        fn = env.Getenv("TI_TFSTATE")
+        if fn != "" {
+                return fn
+        }
 
-	fn = "terraform.tfstate"
-	_, err := fs.Stat(fn)
-	if err == nil {
-		return fn
-	}
+        contents, err := ioutil.ReadFile(".terraform/environment")
+        if err == nil {
+                fn = fmt.Sprintf("terraform.tfstate.d/%s/terraform.tfstate", contents)
+                _, err := fs.Stat(fn)
+                if err == nil {
+                        return fn
+                }
+        }
 
-	return "."
+        fn = "terraform.tfstate"
+        _, err = fs.Stat(fn)
+        if err == nil {
+                return fn
+        }
+
+        return "."
 }
